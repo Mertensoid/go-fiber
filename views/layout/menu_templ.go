bd18c83393fbe67b6f30b02c7355d98b6a3ef30e
Mertensoid <matsoukm@yandex.ru>
2025-08-02 15:36:54 +0300

    global styles

--- views/layout/menu_templ.go
+++ views/layout/menu_templ.go
@@ -29,7 +29,40 @@ func Menu() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<style>\n        .menu {\n            display: flex;\n            flex-direction: row;\n            justify-content: space-between;\n            padding: 20px 0;\n        }\n        .menu__right {\n            display: flex;\n            gap: 20px;\n        }\n        .menu__right-link {\n            text-decoration: none;\n            color: #FFFFFF;\n            font-size: 16px;\n            cursor: pointer;\n        }\n        .menu__right-link:hover {\n            color: #9f9f9f;\n        }\n    </style><div class=\"menu\"><div class=\"menu__logo\">Лого</div><div class=\"menu__right\"><a class=\"menu__right-link\">Войти</a></div></div>")
+		templ_7745c5c3_Err = MenuStyle().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"menu\"><div class=\"menu__logo\">Лого</div><div class=\"menu__right\"><a class=\"menu__right-link\">Войти</a></div></div>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		return nil
+	})
+}
+
+func MenuStyle() templ.Component {
+	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
+			return templ_7745c5c3_CtxErr
+		}
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+		if !templ_7745c5c3_IsBuffer {
+			defer func() {
+				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err == nil {
+					templ_7745c5c3_Err = templ_7745c5c3_BufErr
+				}
+			}()
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var2 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var2 == nil {
+			templ_7745c5c3_Var2 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<style>\n        .menu {\n            display: flex;\n            flex-direction: row;\n            justify-content: space-between;\n            padding: 20px 0;\n        }\n        .menu__right {\n            display: flex;\n            gap: 20px;\n        }\n        .menu__right-link {\n            text-decoration: none;\n            color: #FFFFFF;\n            font-size: 16px;\n            cursor: pointer;\n        }\n        .menu__right-link:hover {\n            color: #9f9f9f;\n        }\n    </style>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
