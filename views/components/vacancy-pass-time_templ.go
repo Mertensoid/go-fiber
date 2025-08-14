71e9953851c7f88fc0dda79bff09684bb45770d9
Mertensoid <matsoukm@yandex.ru>
2025-08-14 16:44:48 +0300

    elapsed lib with changes Изменил код библиотеки elapsed для корректного отображения времени с момента размещения вакансии

--- views/components/vacancy-pass-time_templ.go
+++ views/components/vacancy-pass-time_templ.go
@@ -9,6 +9,7 @@ import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
 import "time"
+import "github.com/rvflash/elapsed"
 
 func VacancyPassTime(time time.Time) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
@@ -31,6 +32,7 @@ func VacancyPassTime(time time.Time) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
+		date := elapsed.LocalTime(time, "ru")
 		templ_7745c5c3_Err = VacancyPassTimeStyle().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
@@ -40,9 +42,9 @@ func VacancyPassTime(time time.Time) templ.Component {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var2 string
-		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(time.String())
+		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(date)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/components/vacancy-pass-time.templ`, Line: 8, Col: 22}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/components/vacancy-pass-time.templ`, Line: 10, Col: 13}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
